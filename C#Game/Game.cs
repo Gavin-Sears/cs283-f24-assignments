ce86bbd2ec06faf86b1d071ca7f7b21173302d0a
Sears <sgsears@haverford.edu>
2024-09-11 22:47:57 -0400

    assignment 2 started

--- C#Game/Game.cs
+++ C#Game/Game.cs
@@ -1,19 +1,107 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
 using System.Drawing;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Principal;
 using System.Windows.Forms;
+using static Physics2D;
+using static Physics2D.Vector2;
 
 public class Game
 {
+    public Player p1;
+    public Block b1;
+
+    public class Player
+    {
+        public PhysicsSphere _PS;
+        //public Image mImg;
+
+        public Player()
+        {
+            this._PS = new PhysicsSphere(
+                    5,
+                    1,
+                    new Vector2(Window.width / 2.0f, Window.height / 2.0f),
+                    new Vector2(0.0f, 19.6f),
+                    new Vector2(0.0f, 0.0f),
+                    new Vector2(0.0f, 0.0f),
+                    0.0f
+                );
+        }
+
+        public Vector2 getPosition()
+        {
+            return this._PS._position;
+        }
+
+        public float getRadius()
+        {
+            return this._PS._radius;
+        }
+
+        public void Update(float dt)
+        {
+            this._PS.Update(dt);
+        }
+    }
+
+    public class Block
+    {
+        public CollisionRect _CR;
+        //public Image mImg;
+
+        public float _height { get; }
+        public float _length { get; }
+        public Vector2 _position { get; }
+
+        public Block(float l, float h, Vector2 p)
+        {
+            this._height = h;
+            this._length = l;
+
+            this._position = p;
+
+            this._CR = new CollisionRect(
+                    l,
+                    h,
+                    p
+                );
+        }
+
+        public void Update(float dt)
+        {
+        }
+    }
+
     public void Setup()
     {
+        p1 = new Player();
+        b1 = new Block(
+                100,
+                20,
+                new Vector2(Window.width / 2.0f, Window.height * (3.0f / 4.0f))
+            );
     }
 
     public void Update(float dt)
     {
+        p1._PS.CollideRect(b1._CR);
+        Console.WriteLine(p1._PS._velocity);
+        p1.Update(dt);
     }
 
     public void Draw(Graphics g)
     {
+        Color c = Color.FromArgb(100, 250, 0, 0); // red
+        Brush brush = new SolidBrush(c);
+        Vector2 ppos = p1.getPosition();
+        g.FillEllipse(brush, ppos.X, ppos.Y, 10, 10);
+
+        Color blue = Color.FromArgb(100, 0, 0, 250);
+        Brush brush2 = new SolidBrush(blue);
+        Vector2 bpos = b1._position;
+        g.FillRectangle(brush2, bpos.X, bpos.Y, b1._length, b1._height);
     }
 
     public void MouseClick(MouseEventArgs mouse)
---
+++ C#Game/Physics2D.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Security.Authentication.ExtendedProtection;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+public static class Physics2D
+{
+	public class PhysicsSphere
+	{
+		public float _radius { get; set; }
+		private float _mass { get; set; }
+        public Vector2 _position { get; set; }
+        private Vector2 _force { get; set; }
+		private Vector2 _acceleration { get; set; }
+		public Vector2 _velocity { get; set; }
+
+		private float _decay { get; set; }
+
+		public PhysicsSphere(
+			float r,
+			float m,
+			Vector2 p,
+			Vector2 f,
+			Vector2 a,
+			Vector2 v,
+			float d = 0.0f
+			)
+		{
+			this._radius = r;
+			this._mass = m;
+			this._position = p;
+			this._force = f;
+			this._acceleration = a;
+			this._velocity = v;
+			this._decay = d;
+		}
+
+
+        public void ApplyForce()
+		{
+			this._acceleration = this._force * this._mass;
+			this._force -= this._force * this._decay;
+		}
+
+		public void ApplyAcceleration(float dt)
+		{
+			this._velocity += this._acceleration * dt;
+		}
+
+		public void ApplyVelocity(float dt)
+		{
+			this._position += this._velocity * dt;
+		}
+
+		public void UpdateBasicPhysics(float dt)
+		{
+			this.ApplyForce();
+			this.ApplyAcceleration(dt);
+			this.ApplyVelocity(dt);
+		}
+
+		// Need to figure out direction of normal force
+		public void CollideRect(CollisionRect rect)
+        {
+            Vector2 rectPos = rect._position;
+
+            float distX = Math.Abs(this._position.X - rectPos.X);
+            float distY = Math.Abs(this._position.Y - rectPos.Y);
+
+			float normX;
+			if (distX > 0.0)
+			{
+				normX = distX / Math.Abs(distX);
+			}
+			else
+			{
+				normX = 0.0f;
+			}
+			float normY;
+			if (distY > 0.0)
+			{
+               normY = distY / Math.Abs(distY);
+            }
+			else
+			{
+				normY = 0.0f;
+			}
+
+			float oldX = this._velocity.X;
+			float oldY = this._velocity.Y;
+			float newX = this._velocity.X * normX;
+			float newY = this._velocity.Y * normY;
+
+            if (distX > ((rect._length / 2.0f) + this._radius)) { return; }
+            if (distY > ((rect._height / 2.0f) + this._radius)) { return; }
+
+            if (distX <= rect._length / 2.0f) {
+				this._velocity = new Vector2(newX, oldY);
+			}
+
+            if (distY <= rect._height / 2.0f) {
+				this._velocity = new Vector2(oldX, newY);
+			}
+
+            double squaredDist = Math.Pow((double)(distX - (rect._length / 2.0f)), 2.0) +
+                Math.Pow((double)(distY - (rect._height / 2.0f)), 2.0);
+
+            if (squaredDist <= (this._radius * this._radius))
+			{
+				if (distX < distY)
+                {
+                    this._velocity = new Vector2(newX, oldY);
+                }
+				else
+                {
+                    this._velocity = new Vector2(oldX, newY);
+                }
+			}
+        }
+
+		public bool IntersectRect(CollisionRect rect)
+        {
+            Vector2 rectPos = rect._position;
+
+			float distX = Math.Abs(this._position.X - rectPos.X);
+			float distY = Math.Abs(this._position.Y - rectPos.Y);
+
+			if (distX > ((rect._length / 2.0f) + this._radius)) { return false; }
+			if (distY > ((rect._height / 2.0f) + this._radius)) { return false; }
+
+			if (distX <= rect._length / 2.0f) { return true; }
+			if (distY <= rect._height / 2.0f) { return true; }
+
+			double squaredDist = Math.Pow((double) (distX - (rect._length / 2.0f)), 2.0) + 
+				Math.Pow((double) (distY - (rect._height / 2.0f)), 2.0);
+
+			return (squaredDist <= (this._radius * this._radius));
+        }
+
+		public void Update(float dt)
+		{
+			this.UpdateBasicPhysics(dt);
+		}
+	}
+
+	public class CollisionRect
+	{
+		public float _length { get; set; }
+		public float _height { get; set; }
+
+        public Vector2 _position { get; set; }
+
+        public float _left { get; set; }
+        public float _right { get; set; }
+        public float _top { get; set; }
+        public float _bottom { get; set; }
+
+        // Top left, top right,
+        // bottom left, bottom right
+        private Vector2[] _corners;
+
+		public CollisionRect(float l, float h, Vector2 pos)
+		{
+			this._length = l;
+			this._height = h;
+			this._position = pos;
+
+			this._left = pos.X - (l / 2.0f);
+            this._right = pos.X + (l / 2.0f);
+            this._top = pos.Y - (h / 2.0f);
+            this._bottom = pos.Y + (h / 2.0f);
+
+
+            this._corners = [
+					new Vector2(_left, _top),
+                    new Vector2(_right, _top),
+                    new Vector2(_left, _bottom),
+                    new Vector2(_right, _bottom)
+				];
+		}
+	}
+
+    /*
+	 * I have used code from online for this struct
+	 * because it did not make sense to code this
+	 * myself. Code based on stack overflow user
+	 * https://stackoverflow.com/users/2125720/jayram
+	 * post: https://stackoverflow.com/questions/17249919/c-sharp-vector2-code
+	 */
+    public struct Vector2
+	{
+		public float X;
+		public float Y;
+
+		public Vector2(float x, float y)
+		{
+			this.X = x;
+			this.Y = y;
+        }
+
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector2 operator *(Vector2 v1, float m)
+        {
+            return new Vector2(v1.X * m, v1.Y * m);
+        }
+
+        public static Vector2 operator /(Vector2 v1, float m)
+        {
+            return new Vector2(v1.X / m, v1.Y / m);
+        }
+
+        public static float Distance(Vector2 v1, Vector2 v2)
+        {
+            return (float)Math.Sqrt(Math.Pow(v1.X - v2.X, 2) + Math.Pow(v1.Y - v2.Y, 2));
+        }
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(X * X + Y * Y);
+        }
+
+        public override string ToString()
+        {
+			return "{ " + this.X + ", " + this.Y + " }";
+        }
+    }
+}
