bd35e271313115759e153d66f2f2e0a1fde61ea7
Sears <sgsears@haverford.edu>
2024-09-17 16:32:02 -0400

    created scene for lab

---
+++ HelloUnity/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("control parameters")]
+    [SerializeField]
+    private float rotateSpeed;
+	[SerializeField]
+	private float speed;
+
+    private Transform tr;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+		Vector3 normMovement = (new Vector3(inputX, 0.0f, inputZ)).normalized;
+
+		float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        yRotation += mouseX * rotateSpeed * Time.deltaTime;
+        xRotation += -Mathf.Clamp(mouseY, -90f, 90) * rotateSpeed * Time.deltaTime;
+
+        tr.rotation = Quaternion.Euler(new Vector3(xRotation, yRotation, 0.0f));
+
+        tr.position += 
+            speed * 
+            Time.deltaTime * 
+            ((tr.forward * normMovement.z) + (tr.right * normMovement.x));
+    }
+}
