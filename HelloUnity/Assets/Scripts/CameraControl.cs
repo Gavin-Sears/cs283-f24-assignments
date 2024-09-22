8e8780ed0db070c5502acc713e221cf1a31bbe0b
Gavin-Sears <sgsears@haverford.edu>
2024-09-22 01:17:24 -0400

    Moved files and added cursor-hiding script

--- HelloUnity/Assets/Scripts/CameraControl.cs
+++ HelloUnity/Assets/Scripts/CameraControl.cs
@@ -17,6 +17,7 @@ public class CameraControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = false;
         tr = GetComponent<Transform>();
     }
 
