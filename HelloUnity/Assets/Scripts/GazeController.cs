c1d9a3ad167498d80db7cd0ec2aed818ddcf7b1e
Gavin-Sears <sgsears@haverford.edu>
2024-10-22 15:15:49 -0400

    fixed delay in IK controllers

--- HelloUnity/Assets/Scripts/GazeController.cs
+++ HelloUnity/Assets/Scripts/GazeController.cs
@@ -28,7 +28,7 @@ public class GazeController : MonoBehaviour
     public static void IKRotate(ref Transform tr, Vector3 trForward, Transform target)
     {
         // e - target of rotation - difference from player (target - transform)
-        Vector3 e = (target.position - tr.position);
+        Vector3 e = (target.position - (tr.position + trForward));
         // r - limb we are rotating forward vector of transform
         Vector3 r = trForward;
 
@@ -40,27 +40,22 @@ public class GazeController : MonoBehaviour
         Debug.DrawLine(tr.position, tr.position + cross, new Color(0.0f, 0.0f, 1.0f));
 
         // phi = atan2(||r x e||, (r dot r) + (r dot e))
-        float theta = Mathf.Atan2(cross.magnitude, Mathf.Abs(Vector3.Dot(r, r) + Vector3.Dot(r, e)));
+        float theta = Mathf.Atan2(cross.magnitude, Vector3.Dot(r, r) + Vector3.Dot(r, e));
         // noramlize the cross product
         Vector3 axis = cross.normalized;
 
-        tr.Rotate(axis, theta);
-        // in order to fix backwards eye glitch, will need to uses cases like this
-        // (could need to be for whole equation, though!)
-        /*
-        if (Vector3.Dot(r, r) + Vector3.Dot(r, e) > 0.0f)
-            tr.Rotate(axis, theta);
-        else
-            tr.Rotate(axis, theta);
-        */
+        Quaternion rot = findQuaternion(theta, axis);
+
+        tr.rotation = tr.rotation * rot;
     }
 
     // finds quaternion from an axis of rotation and a theta value
     // turns out this was not needed, but I'll keep it anyways
     public static Quaternion findQuaternion(float theta, Vector3 axis)
     {
+        Vector3 normAxis = axis.normalized;
         float halfAngle = theta / 2.0f;
         float sinHA = Mathf.Sin(halfAngle);
-        return new Quaternion(sinHA * axis.x, sinHA * axis.y, sinHA * axis.z, Mathf.Cos(halfAngle));
+        return new Quaternion(sinHA * normAxis.x, sinHA * normAxis.y, sinHA * normAxis.z, Mathf.Cos(halfAngle));
     }
 }
